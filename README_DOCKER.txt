7a6ca934019c51a5bfcf1b2ac3c8221c661c8285
kwakd <kwakd.oregonstate.edu>
2024-01-02 23:54:38 -0800

    uploaded files

---
+++ README_DOCKER.txt
@@ -0,0 +1,2 @@
+1. docker build -t assignment_1 .
+2. docker run -d --name assignment_1 -p 7975:7975 assignment_1
---
+++ global.js
@@ -0,0 +1,12 @@
+var businx = [];
+// var businx = new Map();
+var pageSize = 10;
+var reviewx = {};
+var photox = {};
+
+module.exports = {
+    businx,
+    pageSize,
+    reviewx,
+    photox
+}
